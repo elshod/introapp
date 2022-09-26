61d45bc305c6d756eff05333142a90d0cff6e6e3
elshod <elshod5f@gmail.com>
2022-09-26 10:39:48 +0500

    footerga yangi qator menyular qo'shildi

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -22,6 +22,13 @@ const Footer = () => {
                 <li><a href="#">Link 4</a></li>
                 <li><a href="#">Link 5</a></li>
             </ul>
+            <ul className="footer__menu">
+                <li><a href="#">Link 1</a></li>
+                <li><a href="#">Link 2</a></li>
+                <li><a href="#">Link 3</a></li>
+                <li><a href="#">Link 4</a></li>
+                <li><a href="#">Link 5</a></li>
+            </ul>
         </footer>
     )
 }
